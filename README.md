d31e3259c8252a5a1792a2cd8aa43b2f526b667f
Peter Upfold <pgithub@upfold.org.uk>
2019-06-09 09:13:07 +0100

    Sends UserNotifications

---
+++ README.md
@@ -0,0 +1,10 @@
+SockyNotifier
+=============
+
+Throw a macOS notification when connection attempts to servers on the local host are observed.
+
+This is designed to provide some level of visibility into any activities on your system where local servers
+running on Well-Known (and merely well-known, if you catch my drift) ports are being enumerated.
+
+Inspired by the proof of concept of this demonstrated by https://wybiral.github.io/wtf/
+
--- SockyNotifier/AppDelegate.swift
+++ SockyNotifier/AppDelegate.swift
@@ -1,12 +1,23 @@
-//
-//  AppDelegate.swift
-//  SockyNotifier
-//
-//  Created by Peter Upfold on 09/06/2019.
-//  Copyright © 2019 Peter Upfold. All rights reserved.
-//
+/*
+ * Socky.
+ *
+ * Copyright (C) 2019 Peter Upfold.
+ *
+ * This file is licensed to you under the Apache License, version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
 
 import Cocoa
+import UserNotifications
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,11 +26,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        let center = UNUserNotificationCenter.current()
+        // Request permission to display alerts and play sounds.
+        center.requestAuthorization(options: [.alert, .sound])
+        { (granted, error) in
+            debugPrint(granted)
+            debugPrint(error)
+        }
+        
+        DistributedNotificationCenter.default.addObserver(self, selector:#selector(self.didReceiveNotification), name: Notification.Name("uk.org.upfold.Socky.ReceivedConnection"), object: nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    @objc func didReceiveNotification(notification: Notification) {
+        debugPrint(notification)
+        let content = UNMutableNotificationContent()
+        content.title = "Socky"
+        content.body = notification.description
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                // Handle any errors.
+            }
+        }
+
     }
 
 
