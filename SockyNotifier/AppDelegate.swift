3bb3a70eeeffb139f7909bb73dda236a672a1cc5
Peter Upfold <pgithub@upfold.org.uk>
2019-06-09 08:56:53 +0100

    Initial Commit

---
+++ SockyNotifier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SockyNotifier
+//
+//  Created by Peter Upfold on 09/06/2019.
+//  Copyright © 2019 Peter Upfold. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
